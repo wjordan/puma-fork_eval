0acb1be1653f4113e43ed0b6fa2518cd0615c602
Will Jordan <will.jordan@gmail.com>
2021-03-18 16:36:50 -0700

    Refactoring to reduce method length

--- lib/puma/fork_eval.rb
+++ lib/puma/fork_eval.rb
@@ -5,32 +5,35 @@ require 'digest/md5'
 require 'socket'
 
 module Puma
+  # Client code to trigger fork-eval Ruby code in a preloaded application process.
   module ForkEval
     class Error < StandardError; end
 
     module_function
 
-    def socket_dir
-      ENV['XDG_RUNTIME_DIR'] || '/tmp'
+    def eval(code)
+      socket = open_socket
+      [$stdin, $stdout, $stderr].each(&socket.method(:send_io))
+      socket.write(code)
+      socket.close_write
+      socket.gets
+    end
+
+    def open_socket
+      UNIXSocket.open(socket_path)
+    rescue Errno::ENOENT
+      abort <<~MSG
+        Socket not found: #{socket_path}
+        Ensure the Puma server is running with the fork_eval plugin loaded.
+      MSG
     end
 
     def socket_path
       ENV['FORK_EVAL_SOCKET'] || "#{socket_dir}/fork_eval.sock"
     end
 
-    def eval(code)
-      socket = begin
-        UNIXSocket.open(socket_path)
-      rescue Errno::ENOENT
-        abort <<~MSG
-          Socket not found: #{socket_path}
-          Ensure the Puma server is running with the fork_eval plugin loaded.
-        MSG
-      end
-      [$stdin, $stdout, $stderr].each(&socket.method(:send_io))
-      socket.write(code)
-      socket.close_write
-      socket.gets
+    def socket_dir
+      ENV['XDG_RUNTIME_DIR'] || '/tmp'
     end
   end
 end
--- lib/puma/plugin/fork_eval.rb
+++ lib/puma/plugin/fork_eval.rb
@@ -3,20 +3,28 @@
 require 'puma/plugin'
 require 'puma/fork_eval'
 
+# Puma plugin to add a background thread to listen on a Unix domain socket,
+# executing Ruby code in a forked process for incoming requests.
 Puma::Plugin.create do
   def start(_)
     in_background do
       Socket.unix_server_loop(Puma::ForkEval.socket_path) do |socket|
         socket = UNIXSocket.for_fd(socket.fileno)
-        pid = fork do
-          [$stdin, $stdout, $stderr].each { |io| io.reopen(socket.recv_io) }
-          eval(socket.read)
-        end
         Thread.new do
-          Process.wait(pid)
+          Process.wait fork_eval(socket)
           socket.close
         end
       end
+    end
+  end
+
+  # NOTE: the use of eval indicates a security risk.
+  # Any user that has access to the socket can run Ruby code
+  # that can access a copy of Puma and the loaded application.
+  def fork_eval(socket)
+    fork do
+      [$stdin, $stdout, $stderr].each { |io| io.reopen(socket.recv_io) }
+      eval(socket.read) # rubocop:disable Security/Eval
     end
   end
 end
